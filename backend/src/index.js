5a75746da347606fb6f1b15f8e6ca5bf38ef111e
zhu0lin <zhuolinli91@gmail.com>
2025-08-12 23:37:53 -0400

    Backend redeployment fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ import nycLocationsRouter from './routes/nycLocations.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(helmet());
